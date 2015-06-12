bf0804091a2108aab29967bb3cbc4db9d164c6f0
Geula Katan <gkatan@brandeis.edu>
2015-06-12 14:49:29 -0400

    added startup data

---
+++ server/startup.js
@@ -0,0 +1,13 @@
+Meteor.startup(function(){
+	if (Words.find({}).count()==0){
+		// create some initial data for the collection...
+		Words.insert({theWord:"the"});
+
+		Words.insert({theColor:"dog"});
+		Words.insert({theColor:"sat"});
+		Words.insert({theColor:"on"});
+		Words.insert({theColor:"the"});
+		Words.insert({theColor:"cat"});
+	}
+
+});
