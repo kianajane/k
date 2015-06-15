c0c375b1fa63ac162ada31e0984a91e6629a97ff
Melanie Kaplan-Cohen <mkaplancohen@gmail.com>
2015-06-15 15:47:57 -0400

    modified startup

--- server/startup.js
+++ server/startup.js
@@ -1,11 +1,11 @@
 Meteor.startup(function(){
 	if (Words.find({}).count()==0){		// create some initial data for the collection...
 		Words.insert({theWord:"the"});
-		Words.insert({theColor:"dog"});
-		Words.insert({theColor:"sat"});
-		Words.insert({theColor:"on"});
-		Words.insert({theColor:"the"});
-		Words.insert({theColor:"cat"});
+		Words.insert({theWord:"dog"});
+		Words.insert({theWord:"sat"});
+		Words.insert({theWord:"on"});
+		Words.insert({theWord:"the"});
+		Words.insert({theWord:"cat"});
 	}
 
 });
