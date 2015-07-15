4689b986f7a9eb52f55effe57841a00775e28367
Melanie Kaplan-Cohen <mkaplancohen@gmail.com>
2015-07-15 13:44:49 -0400

    login stugg

--- client/welcome.js
+++ client/welcome.js
@@ -1,3 +1,5 @@
+r = true;
+
 Template.login.events({
 
     'submit #login_form': function(event){
@@ -13,6 +15,39 @@ Template.login.events({
           Router.go('/');
         }
       });
-         return false; 
+      return false; 
+    },
+    'click #new_user': function(event){
+      r = false;
+    }
+});
+
+Template.register.events({
+
+    'submit #register_form': function(event){
+      event.preventDefault();
+
+      var email = event.target.email.value;
+      var password = event.target.password.value;
+      var password2 = event.target.password2.value;
+      if (password!=password2){
+        $("#mssg").html("Passwords do not match, try again")
       }
-  });
+      
+      Accounts.createUser({email: email, password: password}, function(err){
+        if (err){
+          $("#mssg").html("Account creation failed")
+        } else {
+          Router.go('/profileEdit');
+        }
+      });
+      return false; 
+    }
+});
+
+Template.welcome.helpers({
+  profile: function(){ 
+    return Meteor.users.findOne({_id: Meteor.userId()}).profile
+  },
+  existingUser: r
+})
