0998583bc086072d47983a4c03706dc1a1deda3f
Geula Katan <gkatan@brandeis.edu>
2015-07-20 10:49:06 -0400

    fixed login problem?

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -4,4 +4,4 @@ Meteor.subscribe("theHistory"); // All of the history for every user.
 
 
 //Meteor.subscribe("theWords"); // Delete after July 20th if still unused.
-//Meteor.subscribe("theProfiles"); // Do we use this?? // Delete after July 20th if still unused.
+Meteor.subscribe("theProfiles"); // Do we use this?? // Delete after July 20th if still unused.
