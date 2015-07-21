6124169cdb7377d5c9ff931c29b965a2efdd684d
Geula Katan <gkatan@brandeis.edu>
2015-07-21 13:52:18 -0400

    fixed previous status thing

--- client/Welcome/welcome.js
+++ client/Welcome/welcome.js
@@ -73,6 +73,6 @@ Template.welcome.helpers({
   },
 
   last: function() {
-    return History.findOne({userId: Meteor.userId()},{sort:{time:1}});
+    return History.findOne({userId: Meteor.userId()},{sort:{time:-1}});
   }
 })
