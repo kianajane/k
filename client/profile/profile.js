99a205a97eb2d80d1eca0db85332fe8663ffe3e6
Geula Katan <gkatan@brandeis.edu>
2015-07-21 10:18:49 -0400

    profile cleanup

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -117,7 +117,7 @@ function builtColumn()
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.0f} Correct</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
