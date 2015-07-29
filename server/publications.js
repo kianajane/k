d085479ebf0d1ccefaffa282d956a90a25c42979
Geula Katan <gkatan@brandeis.edu>
2015-07-29 13:53:55 -0400

    only publishs the user's history

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
 Meteor.publish("thePhonetics",function(){return Phonetics.find();}); // All of the phonetics data, including words and the story.
-Meteor.publish("theHistory",function(){return History.find();}); // Only that user's history. // Adding only that user's history didn't work....??
+Meteor.publish("theHistory",function(){
+    return History.find({userId: this.userId});
+	this.ready();
+}); // Only that user's history. // Adding only that user's history didn't work....??
+
 Meteor.publish("theProfiles",function(){return Profiles.find();}); // Stores the data for each login.
 
 Meteor.publish("userData", function () {
